1cdd9f75ad87b90aa430f0833575a4e285e397d6
hustf <hustf@users.noreply.github.com>
2021-03-26 18:09:36 +0100

    modified:   Project.toml v0.1.2 modified:   src/MechGlueDiffEqBase.jl Drop __init__()

--- src/MechGlueDiffEqBase.jl
+++ src/MechGlueDiffEqBase.jl
@@ -1,24 +1,25 @@
 module MechGlueDiffEqBase
-
-using MechanicalUnits
-import MechanicalUnits.Unitfu as Unitful
-import DiffEqBase
+import MechanicalUnits.Unitfu.AbstractQuantity
 import DiffEqBase: value, ODE_DEFAULT_NORM, UNITLESS_ABS2
+export value, ODE_DEFAULT_NORM, UNITLESS_ABS2, Unitfu
 
-function __init__()
-    # Glue code copied from DiffEqBase.jl init.jl
-    # Here, we are using an alias. We are actually using types from Unitfu, but
-    # with the alias Unitful. Unitfu should only vary from Unitful in how it shows and parses quantities.
-    value(x::Type{Unitful.AbstractQuantity{T,D,U}}) where {T,D,U} = T
-    value(x::Unitful.AbstractQuantity) = x.val
-    @inline function ODE_DEFAULT_NORM(u::AbstractArray{<:Unitful.AbstractQuantity,N},t) where {N}
-        sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
-    end
-    @inline function ODE_DEFAULT_NORM(u::Array{<:Unitful.AbstractQuantity,N},t) where {N}
-        sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
-    end
-    @inline ODE_DEFAULT_NORM(u::Unitful.AbstractQuantity,t) = abs(value(u))
-    @inline UNITLESS_ABS2(x::Unitful.AbstractQuantity) = real(abs2(x)/oneunit(x)*oneunit(x))
+function value(x::Type{AbstractQuantity{T,D,U}}) where {T,D,U}
+    T
+end
+function value(x::AbstractQuantity)
+    x.val
+end
+@inline function ODE_DEFAULT_NORM(u::AbstractArray{<:AbstractQuantity,N},t) where {N}
+    sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
+end
+@inline function ODE_DEFAULT_NORM(u::Array{<:AbstractQuantity,N},t) where {N}
+    sqrt(sum(x->ODE_DEFAULT_NORM(x[1],x[2]),zip((value(x) for x in u),Iterators.repeated(t))) / length(u))
+end
+@inline function ODE_DEFAULT_NORM(u::AbstractQuantity,t)
+    abs(value(u))
+end
+@inline function UNITLESS_ABS2(x::AbstractQuantity)
+    real(abs2(x)/oneunit(x)*oneunit(x))
 end
 
 
