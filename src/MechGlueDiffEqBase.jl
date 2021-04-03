11bf60e230f169f81bb64bef94a4e78cbbc389b9
hustf <hustf@users.noreply.github.com>
2021-04-03 14:46:50 +0200

    WIP AbstractTimeseriesSolution plot recipe modified:   Project.toml modified:   src/MechGlueDiffEqBase.jl new file:   test/Manifest.toml new file:   test/Project.toml modified:   test/runtests.jl

--- src/MechGlueDiffEqBase.jl
+++ src/MechGlueDiffEqBase.jl
@@ -1,6 +1,10 @@
 module MechGlueDiffEqBase
-import MechanicalUnits.Unitfu.AbstractQuantity
+import Unitfu: AbstractQuantity, Quantity
 import DiffEqBase: value, ODE_DEFAULT_NORM, UNITLESS_ABS2
+import SciMLBase
+import SciMLBase: AbstractTimeseriesSolution, RecipesBase
+import SciMLBase: AbstractDiscreteProblem, AbstractRODESolution, SensitivityInterpolation
+import RecipesBase.@recipe
 export value, ODE_DEFAULT_NORM, UNITLESS_ABS2, Unitfu
 
 function value(x::Type{AbstractQuantity{T,D,U}}) where {T,D,U}
@@ -21,6 +25,29 @@ end
 @inline function UNITLESS_ABS2(x::AbstractQuantity)
     real(abs2(x)/oneunit(x)*oneunit(x))
 end
+
+
+@recipe function f(sol::AbstractTimeseriesSolution{T, N, A};
+    plot_analytic=false,
+    denseplot = (sol.dense ||
+                 typeof(sol.prob) <: AbstractDiscreteProblem) &&
+                 !(typeof(sol) <: AbstractRODESolution) &&
+                 !(hasfield(typeof(sol),:interp) &&
+                   typeof(sol.interp) <: SensitivityInterpolation),
+    plotdensity = min(Int(1e5),sol.tslocation==0 ?
+                 (typeof(sol.prob) <: AbstractDiscreteProblem ?
+                 max(1000,100*length(sol)) :
+                 max(1000,10*length(sol))) :
+                 1000*sol.tslocation),
+    tspan = nothing, axis_safety = 0.1,
+    vars=nothing) where {T<:Quantity, N, A<:Array{<:Quantity}}
+
+    print("----------------------------------MechGlueDiffEqBase")
+    return Unifu.ustrip(sol.u), Unifu.ustrip(sol.t)
+end
+        # T = Quantity{Float64,  ᴸ∙ ᴹ∙ ᵀ⁻², Unitfu.FreeUnits{(N,),  ᴸ∙ ᴹ∙ ᵀ⁻², nothing}}
+    # N = 1
+    # A = Vector{Quantity{Float64,  ᴸ∙ ᴹ∙ ᵀ⁻², Unitfu.FreeUnits{(N,),  ᴸ∙ ᴹ∙ ᵀ⁻², nothing}}}
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,45 @@
 using MechGlueDiffEqBase
+import MechanicalUnits: @import_expand, Quantity, Time, Force, ∙, Quantity
+import Unitfu.AbstractQuantity
+import DifferentialEquations
+import DifferentialEquations: SciMLBase, solve, ODEProblem, Tsit5
+import SciMLBase
+import SciMLBase: AbstractTimeseriesSolution, RecipesBase
+import SciMLBase: AbstractDiscreteProblem, AbstractRODESolution, SensitivityInterpolation
 using Test
-
-@testset "MechGlueDiffEqBase.jl" begin
-    # Write your tests here.
+import Plots.plot
+@import_expand(N, s)
+@testset "ODE" begin
+    f = (y, p, t) -> 0.5y / 3.0s  # The derivative, yeah
+    u0 = 1.5N
+    tspan = (0.0s, 1.0s)
+    prob = ODEProblem(f, u0, tspan)
+    solve(prob, Tsit5())
+    sol = solve(prob)
+    @test sol.t isa Vector{<:Time}
+    @test sol.u isa Vector{<:Force}
 end
+
+@testset "Plot ODE quantity" begin
+    SciMLBase.RecipesBase.debug(true)
+    f1 = (y, p, t) -> -1.5y / 0.3s  # The derivative, yeah
+    u0 = 1.5N
+    tspan = (0.0s, 1.0s)
+    prob = ODEProblem(f1, u0, tspan)
+    solve(prob, Tsit5())
+    sol = solve(prob)
+    @test sol isa AbstractTimeseriesSolution{T, N, A} where {T, N, A}
+    @test sol isa AbstractTimeseriesSolution{T, N, A} where {T<:AbstractQuantity, N, A}
+    @test sol isa AbstractTimeseriesSolution{T, N, A} where {T<:AbstractQuantity, N, A<:AbstractArray{<:AbstractQuantity}}
+    @test sol isa AbstractTimeseriesSolution{T, N, A} where {T<:Quantity, N, A<:Array{<:Quantity}}
+    plot(sol)
+    # TODO implement in MechGlueDiffEqBase
+    # the recipe found in SciMLBase/solutions/solution_interface.jl:151
+    # Type to dispatch on:
+    # AbstractTimeseriesSolution{T, N, A}) where {T, N, A}
+    # T = Quantity{Float64,  ᴸ∙ ᴹ∙ ᵀ⁻², Unitfu.FreeUnits{(N,),  ᴸ∙ ᴹ∙ ᵀ⁻², nothing}}
+    # N = 1
+    # A = Vector{Quantity{Float64,  ᴸ∙ ᴹ∙ ᵀ⁻², Unitfu.FreeUnits{(N,),  ᴸ∙ ᴹ∙ ᵀ⁻², nothing}}}
+
+end
+
